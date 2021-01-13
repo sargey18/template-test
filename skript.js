a72f2b3454711300168c486f64663b233a6737a1
ryan <ryanteachviet@gmail.com>
2021-01-13 15:40:39 +0000

    screen share

--- skript.js
+++ skript.js
@@ -1,1 +1,27 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+//prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        //catch error here 
+        console.log('error in the select media strem');
+    }
+}
+
+button.addEventListener('click', async () => {
+    //Disable Button
+    button.disabled = true; 
+    // Start picture in Picture 
+    await videoElement.requestPictureInPicture() ;
+    //Reset Button 
+    button.disabled = false; 
+});
+
+selectMediaStream();
